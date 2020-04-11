b764296868f4da35bfae578cb813ec4a2a785a12
devuser <santosh.behera@capgemini.com>
2020-04-11 12:09:44 +0530

    New ReactPorject

---
+++ src/redux_basic.js
@@ -0,0 +1,27 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const objState ={
+    counter : 0
+};
+
+
+const rootReducer = (state = objState,action) =>{
+
+    if(action.type=='INC_INCREMENT'){
+        return{
+            ...state,
+            counter : action.value
+        }
+    }
+
+    return state;
+};
+
+const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+store.dispatch({type:'INC_INCREMENT',value:10});
+
+console.log(store.getState());
